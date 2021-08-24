e517c5bb1a611e3da46ca851b21b3362b2f0c63c
Lucas Lourenco <60081738+Keluris@users.noreply.github.com>
2021-08-24 09:43:45 -0300

    Update KLjavaScript.js

--- KLjavaScript.js
+++ KLjavaScript.js
@@ -206,38 +206,42 @@ function Limpar(valor, validos) {
 }
 
 //Formata número tipo moeda usando o evento onKeyDown
-function Formata(campo, tammax, teclapres, decimal) {
-    var tecla = teclapres.keyCode;
-    vr = Limpar(campo.value, "0123456789");
-    tam = vr.length;
-    dec = decimal
-
-    if (tam < tammax && tecla != 8) { tam = vr.length + 1; }
-
-    if (tecla == 8) { tam = tam - 1; }
-
-    if (tecla == 8 || tecla >= 48 && tecla <= 57 || tecla >= 96 && tecla <= 105) {
-
-        if (tam <= dec) { campo.value = vr; }
-
-        if ((tam > dec) && (tam <= 5)) {
-            campo.value = vr.substr(0, tam - 2) + "," + vr.substr(tam - dec, tam);
-        }
-        if ((tam >= 6) && (tam <= 8)) {
-            campo.value = vr.substr(0, tam - 5) + "." + vr.substr(tam - 5, 3) + "," + vr.substr(tam - dec, tam);
-        }
-        if ((tam >= 9) && (tam <= 11)) {
-            campo.value = vr.substr(0, tam - 8) + "." + vr.substr(tam - 8, 3) + "." + vr.substr(tam - 5, 3) + "," + vr.substr(tam - dec, tam);
-        }
-        if ((tam >= 12) && (tam <= 14)) {
-            campo.value = vr.substr(0, tam - 11) + "." + vr.substr(tam - 11, 3) + "." + vr.substr(tam - 8, 3) + "." + vr.substr(tam - 5, 3) + "," + vr.substr(tam - dec, tam);
-        }
-        if ((tam >= 15) && (tam <= 17)) {
-            campo.value = vr.substr(0, tam - 14) + "." + vr.substr(tam - 14, 3) + "." + vr.substr(tam - 11, 3) + "." + vr.substr(tam - 8, 3) + "." + vr.substr(tam - 5, 3) + "," + vr.substr(tam - 2, tam);
-        }
+function formataMoeda(a, e, r, t) {
+    let n = ""
+      , h = j = 0
+      , u = tamanho2 = 0
+      , l = ajd2 = ""
+      , o = window.Event ? t.which : t.keyCode;
+      a.value = a.value.replace('R$ ','');
+    if (n = String.fromCharCode(o),
+    -1 == "0123456789".indexOf(n))
+        return !1;
+    for (u = a.value.replace('R$ ','').length,
+    h = 0; h < u && ("0" == a.value.charAt(h) || a.value.charAt(h) == r); h++)
+        ;
+    for (l = ""; h < u; h++)
+        -1 != "0123456789".indexOf(a.value.charAt(h)) && (l += a.value.charAt(h));
+    if (l += n,
+    0 == (u = l.length) && (a.value = ""),
+    1 == u && (a.value = "R$ 0" + r + "0" + l),
+    2 == u && (a.value = "R$ 0" + r + l),
+    u > 2) {
+        for (ajd2 = "",
+        j = 0,
+        h = u - 3; h >= 0; h--)
+            3 == j && (ajd2 += e,
+            j = 0),
+            ajd2 += l.charAt(h),
+            j++;
+        for (a.value = "R$ ",
+        tamanho2 = ajd2.length,
+        h = tamanho2 - 1; h >= 0; h--)
+            a.value += ajd2.charAt(h);
+        a.value += r + l.substr(u - 2, u)
     }
-
+    return !1
 }
+
 
 //Função validar data
 function ValidaData(campo) {
